6a12f0bd9ce50b1b187a02507d8a327fc2bc0aa8
aaron-zhlg <zhangligao9509@gmail.com>
2025-02-06 03:28:31 -0500

    update version code

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="reliable-gen",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
     install_requires=[
         'openai>=1.50.0',
