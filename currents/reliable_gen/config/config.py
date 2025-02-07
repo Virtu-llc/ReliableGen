a4fa4da24c15a6148fc68e0190f7255fdcc77d85
aaron-zhlg <zhangligao9509@gmail.com>
2025-02-07 00:01:24 -0500

    use o3-mini as default model

--- currents/reliable_gen/config/config.py
+++ currents/reliable_gen/config/config.py
@@ -3,6 +3,6 @@
 class Config:
     def __init__(self, **kwargs):
         self.use_llm = kwargs.get('use_llm', False)
-        self.model = kwargs.get('model', 'gpt-4o')
+        self.model = kwargs.get('model', 'o3-mini')
         self.llm_key = kwargs.get('llm_key', None)
 
