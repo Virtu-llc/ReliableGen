3c795b81f50f5746c38fac39ebae53d57fe01673
aaron-zhlg <zhangligao9509@gmail.com>
2025-02-07 01:18:43 -0500

    force function call

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="reliable-gen",
-    version="0.0.3",
+    version="0.0.4",
     packages=find_packages(),
     install_requires=[
-        'openai>=1.50.0',
+        'openai>=1.60.0',
     ]
 )
